489ce1e178999ddc1f03a0ee67d769ff03bacb9d
Irene <irenes@MacBook-Pro-4.local>
2018-10-01 17:43:54 +0900

    search + insertion start

--- tree.py
+++ tree.py
@@ -1,3 +1,5 @@
+from bisect import insort
+
 TREE_ORDER = 3
 
 class Leaf:
@@ -91,3 +93,47 @@ class Node:
             ret += "key {}={}\n".format(i, self.keys[i])
 
         return ret
+
+
+def tree_insert(data, root):
+    # Initialize variables
+    n = root
+    stack = []
+
+    # Search where blocks belong.
+    # As a result n is the proper leaf to insert and stack are the parent nodes in case of a split.
+    while type(n) is not Leaf:
+        stack.append(n)
+        q = len(n.children)
+        if data <= n.keys[0]:
+            n = n.children[0]
+        elif data > n.keys[-1]: # -1 = q-2
+            n = n.children[-1] # -1 = q-1
+        else:
+            for i in range(1, len(n.keys)):
+                if data <= n.keys[i]:
+                    n = n.children[i]
+                    break
+
+    # Make sure k is not already inserted in the tree.
+    for leaf_value in n.values:
+        if leaf_value == data:
+            return
+
+    if len(n.values) < TREE_ORDER: # not full
+        insort(n.values, data)
+        return
+
+    # Leaf is full
+    temp = Leaf(values=n.values)
+    insort(temp.values, data)
+    new = Leaf(next_leaf=n.next_leaf)
+    j = len(temp.values) # = p_leaf + 1
+    if j % 2 == 0:
+        j /= 2
+    else:
+        j = (j/2) + 1
+    n = Leaf(values=temp.values[:j], next_leaf=new)
+    new.values = temp.values[j:]
+
+    k = temp.values[j]
